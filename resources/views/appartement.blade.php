c932e00445f1756db452f02fa80ad02a12bf6611
ronantr <noob93500@gmail.com>
2021-07-03 17:30:39 +0200

    tableau appartement.blade.php

--- resources/views/appartement.blade.php
+++ resources/views/appartement.blade.php
@@ -10,11 +10,33 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <ul>
+                        <tr>
+                            <td>ID</td>
+                            <td>Type</td>
+                            <td>nb_piece</td>
+                            <td>localisation</td>
+                            <td>Prix</td>
+                            <td>Surface</td>
+                            <td>Description</td>
+                            <td>Status</td>
+                            <td>Date publication</td>
+                            <td>Date location</td>
+                            <td>ID user</td>
+                            <th >Action</th>
+                        </tr>
                         @foreach ($appartements as $appartement)
                         <li>
-                        
+                            {{ $appartement->id }}
+                            {{ $appartement->type }}
+                            {{ $appartement->nb_piece }}
+                            {{ $appartement->locatisation }}
                             {{ $appartement->prix }}
-
+                            {{ $appartement->surface }}
+                            {{ $appartement->description }}
+                            {{ $appartement->status }}
+                            {{ $appartement->date_publication }}
+                            {{ $appartement->date_location }}
+                            {{ $appartement->id_user }}
                         </li>
                         @endforeach
                     </ul>
