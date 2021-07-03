36ee8088ad90d6d82ca12a44da727b931a9889de
Chaochao <chaochao.zhou@hotmail.com>
2021-07-03 22:55:23 +0200

    adding bootstrap

--- resources/views/appartement.blade.php
+++ resources/views/appartement.blade.php
@@ -9,37 +9,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <ul>
-                        <tr>
-                            <td>ID</td>
-                            <td>Type</td>
-                            <td>nb_piece</td>
-                            <td>localisation</td>
-                            <td>Prix</td>
-                            <td>Surface</td>
-                            <td>Description</td>
-                            <td>Status</td>
-                            <td>Date publication</td>
-                            <td>Date location</td>
-                            <td>ID user</td>
-                            <th >Action</th>
-                        </tr>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Type</th>
+                                <th scope ="col" > nb_piece</th>
+                                <th scope ="col">localisation</th>
+                                <th scope ="col">Prix</th>
+                                <th scope ="col">Surface</th>
+                                <th scope ="col">Description</th>
+                                <th scope ="col">Status</th>
+                                <th scope ="col">Date publication</th>
+                                <th scope ="col">Date location</th>
+                                <th scope ="col">ID user</th>
+                                <th scope ="col" >Action</th>
+                            </tr>
+                        </thead>    
                         @foreach ($appartements as $appartement)
-                        <li>
-                            {{ $appartement->id }}
-                            {{ $appartement->type }}
-                            {{ $appartement->nb_piece }}
-                            {{ $appartement->localisation }}
-                            {{ $appartement->prix }}
-                            {{ $appartement->surface }}
-                            {{ $appartement->description }}
-                            {{ $appartement->status }}
-                            {{ $appartement->date_publication }}
-                            {{ $appartement->date_location }}
-                            {{ $appartement->id_user }}
-                        </li>
+                        <tr class="table-primary">
+                            <td>    {{ $appartement->id }} </td>
+                            <td>    {{ $appartement->type }}</td>
+                            <td>    {{ $appartement->nb_piece }}</td>
+                            <td>    {{ $appartement->localisation }}</td>
+                            <td>    {{ $appartement->prix }}</td>
+                            <td>    {{ $appartement->surface }}</td>
+                            <td>    {{ $appartement->description }}</td>
+                            <td>    {{ $appartement->status }}</td>
+                            <td>    {{ $appartement->date_publication }}</td>
+                            <td>    {{ $appartement->date_location }}</td>
+                            <td>    {{ $appartement->id_user }}</td>
+                        </tr>
                         @endforeach
-                    </ul>
+                    </table>
 
                 </div>
             </div>
